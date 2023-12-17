08ec6fbfe0d89944649b01b5918b35c6af2efa2a
AnumBatool12 <a9batool@gmail.com>
2023-12-17 11:13:16 +0500

    setting up frontend directories

--- frontend/frontend/src/main.jsx
+++ frontend/frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Pages/App.jsx'
-import './CSS/index.css'
+//import './CSS/index.css' will use if needed
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
