6ed8e0b826f2bac3ecbd570f9d0fd1e77c5d278e
AnumBatool12 <a9batool@gmail.com>
2023-12-24 18:14:17 +0500

    item display title and description added

--- frontend/mysportsshop/src/Pages/ItemDisplay.jsx
+++ frontend/mysportsshop/src/Pages/ItemDisplay.jsx
@@ -10,6 +10,54 @@ import React, { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
+//titles and soldout symbol
+function ProductTitle({ data }) {
+    let sumDiscount = data.discount.reduce((acc, disc) => acc + disc, 0)
+    const discounted = ((100 - sumDiscount) * data.price) / 100
+
+    return (
+        <>
+            <p className="sport">{data.sport}</p>
+            <p>
+                <span className="name">{data.name} </span>
+                <span className="collection">({data.Collection})</span>
+            </p>
+
+            {data.discount.length > 0 ? (
+                <p>
+                    <s style={{ color: "red" }}>${data.price} </s>
+                    <span style={{ color: "green" }}> {discounted} each </span>
+                </p>
+            )
+            : (<p>$ {data.price} each</p>)}
+
+            {data.soldout ? (<p style={{ color: "red" }}>Soldout</p>):(<p style={{ color: "green" }}>Stock Available</p>)}
+        </>
+    );
+}
+
+//product description
+function Description({ data }) {
+    return (
+        <>
+            <p>Gender: {data.gender}</p>
+            <p>{data.description}</p>
+        </>
+    );
+}
+
+//reviews and comments and option to do that
+
+
+//color display
+
+//size display
+
+//quantity button
+
+//add to cart option
+
+
 
 
 function ItemDisplay() {
@@ -27,20 +75,15 @@ function ItemDisplay() {
             {isLoading && <div>Item on the Way...</div>}
 
             {data &&
-                <div className="center">
-                    <Container className="itemContainer">
+                <div className="bg">
+                    <Container className="itemContainers">
                         <Row>
                             <Col sm={6}>
                                 <Image src={data.image} rounded className="imagSize padding" />
                             </Col>
                             <Col sm={6}>
                                 <div className="padding">
-                                    <p className="sport">{data.sport}</p>
-                                    <p>
-                                        <span className="name">{data.name} </span>
-                                        <span className="collection">({data.Collection})</span>
-                                    </p>
-                                    <p>$ {data.price} each</p>
+                                    <ProductTitle data={data}></ProductTitle>
                                     <Tabs
                                         id="controlled-tab-example"
                                         activeKey={key}
@@ -49,8 +92,7 @@ function ItemDisplay() {
                                         fill
                                     >
                                         <Tab eventKey="description" title=" Product Description">
-                                            <p>Gender: {data.gender}</p>
-                                            <p>{data.description}</p>
+                                            <Description data={data}></Description>
                                         </Tab>
                                         <Tab eventKey="review" title="Reviews">
                                             //to be done
@@ -58,12 +100,12 @@ function ItemDisplay() {
                                     </Tabs>
                                     <div className="bordertop"></div>
                                     <p>Color</p>
-                                    
+
 
                                     <div className="bordertop"></div>
                                     <p>Sizes</p>
-                                    
-                                    
+
+
                                 //quantity option
                                 //soldout symbol
                                 //Add to Cart option
