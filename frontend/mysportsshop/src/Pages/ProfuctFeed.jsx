49ad3f7d9af9cd5671242c01219966f5e1ec7913
AnumBatool12 <a9batool@gmail.com>
2023-12-25 17:44:17 +0500

    setting up product feed

--- frontend/mysportsshop/src/Pages/ProfuctFeed.jsx
+++ frontend/mysportsshop/src/Pages/ProfuctFeed.jsx
@@ -1,32 +1,63 @@
 import NavBar from "../Components/NavBar";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import useProductCall from "../Hooks/useProductCall";
+import '../CSS/feed.css'
+import { useState } from "react";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import showImage from '../Assets/menuOpen.svg'
+import notshow from '../Assets/menuClosed.svg'
 
-function Display(){
-    const {error, isError, isLoading, data}=useProductCall();
+
+function Display() {
+    const { error, isError, isLoading, data } = useProductCall();
     console.log(data)
     return (
         <>
-        hello
+            hello
         </>
     );
 }
 
-function ProductFeed(){
+function ProductFeed() {
+    const [show, setShow] = useState(true);
+
+    const handleImageClick = () => {
+        setShow(!show); // Toggle the show state on image click
+    };
+
     return (
-        <div>
+        <>
             <NavBar />
-            <Container>
-                <Row>
-                    <Col>h1</Col>
-                    <Col xs={10}>
-                        <Display />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+            <div className="feedflex">
+
+                {show &&
+                    (<div className="filtercol">{/*Div for the Filter portion*/}
+                        louewufherfviebruf
+                    </div>)
+                }
+
+                <div className="prodcol">{/*Div for the Product display portion*/}
+                    <Row>
+                        <Col>
+                            {show &&
+                                <>
+                                    <img alt="Toggle"
+                                        src={showImage}
+                                        onClick={handleImageClick} />
+                                    <span>Hide Filters</span>
+                                </>}
+                            {!show &&
+                                <>
+                                    <img alt="Toggle"
+                                        src={notshow}
+                                        onClick={handleImageClick} />
+                                    <span>Show Filters</span>
+                                </>}
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        </>
     )
 }
 
