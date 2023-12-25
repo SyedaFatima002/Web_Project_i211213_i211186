af68ffd78c2385fad5c7076cd45fb78b17ad6b7b
AnumBatool12 <a9batool@gmail.com>
2023-12-26 01:38:21 +0500

    set up illing details

--- frontend/mysportsshop/src/Pages/Order.jsx
+++ frontend/mysportsshop/src/Pages/Order.jsx
@@ -2,8 +2,9 @@ import useCart from "../Hooks/useCart"
 import NavBar from "../Components/NavBar"
 import useUser from "../Hooks/useUser";
 import useLogin from "../Hooks/useLogin";
-
+import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
+import '../CSS/order.css'
 
 
 function BillingDetails() {
@@ -15,11 +16,61 @@ function BillingDetails() {
                 <h3>Billing Details</h3>
             </div>
             <Form style={{ margin: '3%' }}>
-                
+                <InputGroup className="mb-3 size" style={{ margin: '2%' }}>
+                    <InputGroup.Text id="basic-addon1" className="color">Name</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Recipient's Name"
+                        aria-label="Recipient's name"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+
+                <InputGroup className="mb-3 size" style={{ margin: '2%' }}>
+                    <InputGroup.Text id="basic-addon2" className="color">Phone Number</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Recipient's Phone Number"
+                        aria-label="Recipient's phoneNum"
+                        aria-describedby="basic-addon2"
+                    />
+                </InputGroup>
+
+                <InputGroup className="mb-3 size" style={{ margin: '2%' }}>
+                    <InputGroup.Text id="basic-addon3" className="color">Email</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Recipient's Phone Email"
+                        aria-label="Recipient's email"
+                        aria-describedby="basic-addon3"
+                    />
+                </InputGroup>
+
+                <InputGroup className="mb-3 " style={{ margin: '2%' }}>
+                    <InputGroup.Text id="basic-addon4" className="color">Street Addr</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Street Address"
+                        aria-label="Recipient's address"
+                        aria-describedby="basic-addon4"
+                    />
+
+                    <InputGroup.Text id="basic-addon5" className="color">City</InputGroup.Text>
+                    <Form.Control
+                        placeholder="City"
+                        aria-label="Recipient's city"
+                        aria-describedby="basic-addon5"
+                    />
+
+                    <InputGroup.Text id="basic-addon6" className="color">Country</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Country"
+                        aria-label="Recipient's county"
+                        aria-describedby="basic-addon6"
+                    />
+                </InputGroup>
             </Form>
         </>
     );
 }
+
+
 
 function Order() {
     return (
@@ -32,7 +83,13 @@ function Order() {
             }}>
                 <h1>Welcome to CheckOut</h1>
             </div>
-            <BillingDetails />
+            <div style={{ borderBottom: '2px solid rgb(150, 1, 1)', margin: '2%',}}>
+                <BillingDetails />
+            </div>
+            <div>
+
+            </div>
+            
         </>
     )
 }
